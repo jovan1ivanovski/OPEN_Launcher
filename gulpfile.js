edfe823d555c29c89f45eb9b35623720c0935048
kirceg <kire83@gmail.com>
2016-02-18 14:01:46 +0100

    gulp css bundle task

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
 'use strict';
 
-var gulp       = require('gulp'),
+var gulp = require('gulp'),
     browserify = require('browserify'),
-    source     = require('vinyl-source-stream');
+    source = require('vinyl-source-stream'),
+    minifyCss = require('gulp-minify-css'),
+    concatCss = require('gulp-concat-css');
 
-//Jquery and Bootstrap Bundle
-gulp.task('main-scripts', function() {
+//Scripts Bundle
+gulp.task('main-scripts', function () {
     var files = [
-         './node_modules/jquery/dist/jquery.min.js',
-         './app/js/jqueryelectron.js',
-         './node_modules/bootstrap/dist/js/bootstrap.min.js'
-     ];
+        './node_modules/jquery/dist/jquery.min.js',
+        './app/js/jqueryelectron.js',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/angular2/bundles/angular2-polyfills.min.js'
+    ];
     return browserify({ entries: files })
         .bundle()
-        .pipe(source('scripts.js'))
+        .pipe(source('main-scripts.js'))
         .pipe(gulp.dest('build'));
+});
+
+//CSS Bundle
+gulp.task('main-css', function () {
+    var files = ['./node_modules/bootstrap/dist/css/bootstrap.min.css', './app/styles/site.css'];
+    return gulp.src(files)
+    .pipe(concatCss('main-css.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('build/'));
 });
 
