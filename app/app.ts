52affb1622969b7f5467de155d226f543a5df33d
idimchevski <igor.dimchevski@endava.com>
2016-02-19 16:23:35 +0100

    glyphicons and upload picture added

--- app/app.ts
+++ app/app.ts
@@ -3,16 +3,18 @@ import {bootstrap} from 'angular2/platform/browser';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouterOutlet, LocationStrategy, HashLocationStrategy, APP_BASE_HREF} from 'angular2/router';
 
 import {HomeComponent} from './components/HomeComponent';
+import {UploadPictureComponent} from './components/UploadPictureComponent';
 
 @Component({
     selector: 'app',
     directives: [ROUTER_DIRECTIVES],
-    templateUrl: `./app/views/main.html`,
+    templateUrl: `./app/views/app.html`,
 })
 
 @RouteConfig([
     { path: '/', redirectTo: ['/Home'] },
-    { path: '/home', component: HomeComponent, name: 'Home' }
+    { path: '/home', component: HomeComponent, name: 'Home' },
+    { path: '/uploadpicture', component: UploadPictureComponent, name: 'UploadPicture' },
 ])
 
 export class App { }
---
+++ app/components/UploadPictureComponent.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'uploadPicture',
+    templateUrl: `./app/views/uploadPicture.html`
+})
+
+export class UploadPictureComponent { }
