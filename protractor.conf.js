ad31da2e2e3bcbc99bb61ff5e7df966ddbdb1396
sumarsky <i.dimcevski@gmail.com>
2016-03-03 09:59:07 +0100

    protractor simple example angular2

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,12 @@
 var path = require('path');
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: [path.normalize("app/tests/e2e/example.js")]
+    baseUrl: 'http://localhost:3000/',
+    
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    
+    specs: [path.normalize("app/tests/e2e/example.js")],
+
+    framework: 'jasmine',
+
+    useAllAngular2AppRoots: true
 };
