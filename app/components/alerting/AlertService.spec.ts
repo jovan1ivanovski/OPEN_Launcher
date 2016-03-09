8cf525a7b2adbf7042b69979b872f691b4eee978
sumarsky <i.dimcevski@gmail.com>
2016-03-09 15:33:27 +0100

    Unit tests AlertingService.spec.ts

--- app/components/alerting/AlertService.spec.ts
+++ app/components/alerting/AlertService.spec.ts
@@ -1,14 +1,67 @@
 import {AlertingService} from './AlertingService'
 
-    describe('AlertingService', function () {
-        it('WhenInstanceIsCreated_CurrentAlertsLength_Equals0', function () {
-            var instance = new AlertingService();
-            expect(instance.currentAlerts.length).toEqual(0);
-        });
-        it('WhenInstanceIsCreated_CurrentAlertsLength_Equals0', function () {
-            var instance = new AlertingService();
-            instance.addWarning("addWarning");
-            expect(instance.currentAlerts.length).toEqual(1);
-        });
+describe('AlertingService', function() {
+    var instance: AlertingService = null;
+
+    beforeEach(() => {
+        instance = new AlertingService();
     });
+
+    it('AlertingService_NewInstance_CurrentAlertsLengthIsEqualZero', function() {
+        // Assert
+        expect(instance.currentAlerts.length).toEqual(0);
+    });
+
+    it('AlertingService_addAlert_CurrentAlertsLengthIsEqualOne', function() {
+        // Act
+        instance.addAlert("type", "message");
+
+        // Assert
+        expect(instance.currentAlerts.length).toEqual(1);
+    });
+
+    it('AlertingService_addSuccess_TypeOfTheAddedAlertIsWarning', function() {
+        // Act
+        instance.addSuccess("message");
+
+        // Assert
+        expect(instance.currentAlerts[0].type).toEqual("success");
+    });
+
+    it('AlertingService_addInfo_TypeOfTheAddedAlertIsWarning', function() {
+        // Act
+        instance.addInfo("message");
+
+        // Assert
+        expect(instance.currentAlerts[0].type).toEqual("info");
+    });
+
+    it('AlertingService_addWarning_TypeOfTheAddedAlertIsWarning', function() {
+        // Act
+        instance.addWarning("message");
+
+        // Assert
+        expect(instance.currentAlerts[0].type).toEqual("warning");
+    });
+
+    it('AlertingService_addDanger_TypeOfTheAddedAlertIsWarning', function() {
+        // Act
+        instance.addDanger("message");
+
+        // Assert
+        expect(instance.currentAlerts[0].type).toEqual("danger");
+    });
+
+    it('AlertingService_removeAlert_TypeOfTheAddedAlertIsWarning', function() {
+        // Act
+        instance.addDanger("message");
+        instance.addDanger("message");
+
+        var noAlerts = instance.currentAlerts.length;
+        instance.removeAlert(instance.currentAlerts[0]);
+        
+        // Assert
+        expect(instance.currentAlerts.length).toEqual(noAlerts - 1);
+    });
+});
 
