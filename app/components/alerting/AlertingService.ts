4814d519571925568bd428f21b2907f5d37e0409
idimchevski <igor.dimchevski@endava.com>
2016-03-10 13:47:25 +0100

    alert 3.5 sec lifetime

--- app/components/alerting/AlertingService.ts
+++ app/components/alerting/AlertingService.ts
@@ -10,7 +10,7 @@ export class AlertingService {
     public addAlert = function(type: string, message: string) {
         var alert = new Alert(type, message);
         this.currentAlerts.push(alert);
-        setTimeout(() => this.removeAlert(alert), 10000);
+        setTimeout(() => this.removeAlert(alert), 3500);
     }
     addSuccess(message: string) {
         this.addAlert("success", message);
