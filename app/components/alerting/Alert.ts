1be647f82751c7320912a155e361bd8ff8bbeb4b
sumarsky <i.dimcevski@gmail.com>
2016-03-17 10:43:56 +0100

    deletion of old files (before transfer)

--- app/components/alerting/Alert.ts
+++
@@ -1,5 +0,0 @@
-export class Alert {
-    constructor(public type: string, public message: string) {
-
-    }
-}
--- typings/tsd.d.ts
+++
@@ -1,3 +0,0 @@
-
-/// <reference path="jasmine/jasmine.d.ts" />
-/// <reference path="karma-jasmine/karma-jasmine.d.ts" />
