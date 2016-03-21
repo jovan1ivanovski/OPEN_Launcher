bc5ac748033a800cf7ddf4c004696e97ae257669
sumarsky <i.dimcevski@gmail.com>
2016-03-21 16:33:39 +0100

    getUserSettingsFor unit test

--- src/app/shared/services/UserSettingsService.spec.ts
+++ src/app/shared/services/UserSettingsService.spec.ts
@@ -1,34 +1,84 @@
 import {
-  beforeEach,
-  beforeEachProviders,
-  describe,
-  expect,
   it,
   inject,
-  injectAsync
+  injectAsync,
+  describe,
+  beforeEachProviders,
+  TestComponentBuilder
 } from 'angular2/testing';
-import {BaseRequestOptions, Http, Response} from 'angular2/http';
-import {MockBackend} from 'angular2/http/testing';
-import {provide, Injector} from 'angular2/core';
+
+import {Component, provide} from 'angular2/core';
+import {BaseRequestOptions, Http, Response, ResponseOptions} from 'angular2/http';
+import {MockBackend, MockConnection} from 'angular2/http/testing';
 
 import {UserSettingsService} from './UserSettingsService';
+import {GlobalService} from './GlobalService';
+import * as UserSettingsEnums from '../enums/UserSettingsEnums';
+import {UserSettings} from '../models/UserSettings';
 
-describe('UserSettingsService', function() {
-  var instance: UserSettingsService = null;
-  // ****************************************
-  // var connection;
-  // var injector = Injector.resolveAndCreate([
-  //   MockBackend,
-  //   provide(Http, {useFactory: (backend, options) => {
-  //     return new Http(backend, options);
-  //   }, deps: [MockBackend, BaseRequestOptions]})]);
+describe('UserSettingsService', () => {
 
-  // var http = injector.get(Http);
-  // var backend = injector.get(MockBackend);
-  // backend.connections.subscribe(c => connection = c);
-  // ****************************************
+  function getUserSettingsObject(
+    pointerSize: UserSettingsEnums.PointerSize,
+    pointerType: UserSettingsEnums.PointerType,
+    pointerColor: UserSettingsEnums.PointerColor,
+    backgroundColor: UserSettingsEnums.BackgroundColor
+  ): UserSettings {
+    var result = new UserSettings();
+    result.pointerSize = pointerSize;
+    result.pointerType = pointerType;
+    result.pointerColor = pointerColor;
+    result.backgroundColor = backgroundColor;
+    return result;
+  }
 
-  beforeEach(() => {
-    instance = new UserSettingsService(null, null);
-  });
+  // provide our implementations or mocks to the dependency injector
+  beforeEachProviders(() => [
+    BaseRequestOptions,
+    MockBackend,
+    provide(Http, {
+      useFactory: function(backend, defaultOptions) {
+        return new Http(backend, defaultOptions);
+      },
+      deps: [MockBackend, BaseRequestOptions]
+    }),
+
+    GlobalService,
+    UserSettingsService,
+  ]);
+
+  it('should have http', inject([UserSettingsService], (userSettingsService) => {
+    expect(!!userSettingsService.http).toEqual(true);
+  }));
+
+  it('getUserSettingsFor_ValidUserName_ValidateRetrievedDataMatchFromTheHttpResponse',
+    inject([UserSettingsService, MockBackend], (userSettingsService: UserSettingsService, mockBackend) => {
+      var userSettingsObject = getUserSettingsObject(
+        UserSettingsEnums.PointerSize.Small,
+        UserSettingsEnums.PointerType.Hand,
+        UserSettingsEnums.PointerColor.White,
+        UserSettingsEnums.BackgroundColor.Color
+      );
+
+      mockBackend.connections.subscribe(
+        (connection: MockConnection) => {
+          connection.mockRespond(new Response(
+            new ResponseOptions({
+              body: JSON.stringify(userSettingsObject)
+            }
+            )));
+        });
+
+      userSettingsService.getUserSettingsFor('username').subscribe(
+        (data) => {
+          expect(data.pointerSize).toBe(UserSettingsEnums.PointerSize.Small);
+          expect(data.pointerType).toBe(UserSettingsEnums.PointerType.Hand);
+          expect(data.pointerColor).toBe(UserSettingsEnums.PointerColor.White);
+          expect(data.backgroundColor).toBe(UserSettingsEnums.BackgroundColor.Color);
+        },
+        (error) => {
+          fail(error);
+        }
+      );
+    }));
 });
