9c0af0a246b9e7ee467154939cd1e58d86a98f24
sumarsky <i.dimcevski@gmail.com>
2016-03-21 11:57:59 +0100

    UserSettings class and enums

---
+++ src/app/shared/enums/UserSettingsEnums.ts
@@ -0,0 +1,4 @@
+export enum PointerType { Hand };
+export enum PointerSize { Small, Medium }
+export enum PointerColor { White, Green, Blue, Yellow, Red }
+export enum BackgroundColor { Color, BlackAndWhite }
