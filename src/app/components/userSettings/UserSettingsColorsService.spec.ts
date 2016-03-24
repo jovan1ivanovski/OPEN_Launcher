e9b95d18040ee07e93d5cd62a991dbca7f6700ad
Aleksandra Despotovska <adespotovska@endava.net>
2016-03-24 12:50:56 +0100

    merged the changes

---
+++ src/app/components/userSettings/UserSettingsColorsService.spec.ts
@@ -0,0 +1,28 @@
+import {UserSettingsColorsService} from './UserSettingsColorsService';
+import {PointerColor, BackgroundColor} from '../../shared/enums/UserSettingsEnums';
+
+
+describe('UserSettingsColorsService', function() {
+  var instance: UserSettingsColorsService = null;
+
+  beforeEach(() => {
+    instance = new UserSettingsColorsService();
+  });
+
+  it('UserSettingsColorsService_getPointerColors_ForBlackWhiteColor', function() {
+    // Act
+    var pointerColors = instance.getPointerColors(BackgroundColor.BlackAndWhite);
+
+    // Assert
+    expect(pointerColors.length).toEqual(2);
+  });
+
+  it('UserSettingsColorsService_getPointerColors_ForInColor', function() {
+    // Act
+    var inColorAvailablePointerColor = instance.getPointerColors(BackgroundColor.InColor);
+
+    // Assert
+    expect(inColorAvailablePointerColor.length).toEqual(5);
+  });
+
+})
